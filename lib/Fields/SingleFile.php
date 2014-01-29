073d43348c66cd5461912f9abc1397a2b280524a
Luke Carrier <luke@carrier.im>
2014-01-29 09:40:26 +0000

    Don't save file URLs in their entirety, wp_upload_dir() exists for a reason

--- lib/Fields/SingleFile.php
+++ lib/Fields/SingleFile.php
@@ -15,6 +15,9 @@ class SingleFile extends AField implements IField {
     public function getMetaBoxHtml($post) {
         list($meta_key, $post_key, $value) = $this->getMetaBoxDetails($post->ID);
 
+        $upload_dir = wp_upload_dir();
+        $value = $upload_dir['baseurl'] . '/' . $value;
+
         echo $this->getNonceHtml(),
              '<img src="' . $value . '" alt="" class="haiku-attachment-thumbnail" />',
              '<input type="hidden" name="' . $post_key . '"',
@@ -24,5 +27,30 @@ class SingleFile extends AField implements IField {
              '       data-field-name="' . $post_key . '" value="' . __('Select media') . '" />',
              '<input type="button" class="media-upload-remove-button"',
              '       data-field-name="' . $post_key . '" value="' . __('Remove media') . '" />';
+    }
+
+    public function doSave($post_id, $post) {
+        if (!$this->verifyNonce($post)) {
+            return;
+        }
+
+        $meta_key = $this->getIdentifier();
+        $post_key = $meta_key . '_value';
+        $value    = (array_key_exists($post_key, $_POST)) ? $_POST[$post_key] : NULL;
+
+        $upload_dir = wp_upload_dir();
+        $value = substr($value, strlen($upload_dir['baseurl']) + 1);
+
+        $existing_values = get_post_meta($post->ID, $meta_key);
+        if (count($existing_values) === 1 && $value) {
+            update_post_meta($post->ID, $meta_key, $value);
+            return;
+        } else {
+            delete_post_meta($post->ID, $meta_key);
+        }
+
+        if ($value) {
+            add_post_meta($post->ID, $meta_key, $value);
+        }
     }
 }
