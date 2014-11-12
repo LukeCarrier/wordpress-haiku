f0ae83c70edec45ebbeaad7a8c93bbee93497667
Luke Carrier <luke@carrier.im>
2014-11-12 11:18:47 +0000

    New textarea field type

---
+++ lib/Fields/TextArea.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Haiku: because code is poetry.
+ *
+ * @author Luke Carrier <luke@carrier.im>
+ * @license GPL v3
+ */
+
+namespace Haiku\Fields;
+use Haiku\AField,
+    Haiku\IField;
+
+class TextArea extends AField implements IField {
+    public function getMetaBoxHtmlForPost($post) {
+        list($meta_key, $post_key, $value) = $this->getPostDetails($post->ID);
+        $this->getMetaBox($meta_key, $post_key, $value);
+    }
+
+    public function getMetaBoxHtmlForTerm($term) {
+        list($meta_key, $post_key, $value) = $this->getTermDetails($term->taxonomy,
+                                                                   $term->term_id);
+        $this->getMetaBox($meta_key, $post_key, $value);
+    }
+
+    protected function getMetaBox($meta_key, $post_key, $value) {
+        echo $this->getNonceHtml(),
+             '<textarea name="' . $post_key . '"',
+             'id="' . $post_key . '" class="widefat">' 
+             . $value
+             . '</textarea>';
+    }
+}
